d11b7bcf25cd27bd7e2067b90eacc759389dbc46
hewo233 <2842137843@qq.com>
2024-01-15 20:20:29 +0800

    add Fill

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,5 +11,86 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+@8192
+D=A
+@nums
+M=D // Set the nums to 8192
 
+(LOOP)
+
+    @KBD
+    D=M // Get the keyboard input
+
+    @WHITE
+    D;JEQ
+
+    @BLACK
+    0;JMP
+
+(BLACK)
+    @i
+    M=0 // Set the counter to 0 (use for print)
+
+    @PRINTBLACK
+    0;JMP
+
+(WHITE)
+    @i
+    M=0 // Set the counter to 0 (use for print)
+
+    @PRINTWHITE
+    0;JMP
+
+(PRINTBLACK)
+
+    @nums
+    D=M // Get the nums
+    @i
+    D=D-M // D = nums - i
+    @LOOP
+    D;JEQ // If D = 0, jump to LOOP
+
+    @SCREEN
+    D=A // Set the screen address to D
+
+    @i
+    D=D+M // D = screen + i
+
+    @nowscreen
+    A=D // Set the now address
+    M=-1 // Set the now address to black
+    
+    @i
+    M=M+1 // i++
+
+    @PRINTBLACK
+    0;JMP // Jump to print
+
+(PRINTWHITE)
+
+    @nums
+    D=M // Get the nums
+    @i
+    D=D-M // D = nums - i
+    @LOOP
+    D;JEQ // If D = 0, jump to LOOP
+
+    @SCREEN
+    D=A // Set the screen address to D
+
+    @i
+    D=D+M // D = screen + i
+
+    @nowscreen
+    A=D // Set the now address
+    M=0 // Set the now address to black
+    
+    @i
+    M=M+1 // i++
+
+    @PRINTWHITE
+    0;JMP // Jump to print
+
+
+    
 
